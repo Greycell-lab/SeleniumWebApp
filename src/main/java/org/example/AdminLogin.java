4381367fa8d49ef9405b75cfd9930698e8b14196
dseverin <dseverin@ai-ag.de>
2023-06-07 12:45:14 +0200

    Changed deprecated implicitlyWait of WebDriver

--- src/main/java/org/example/AdminLogin.java
+++ src/main/java/org/example/AdminLogin.java
@@ -4,7 +4,9 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.time.Duration;
 import java.util.concurrent.TimeUnit;
 
 public class AdminLogin {
@@ -17,7 +19,7 @@ public class AdminLogin {
     private void getAdminLogin(){
         System.setProperty("webdriver.chrome.driver", "drivers/chromedriver.exe");
         driver = new ChromeDriver();
-        driver.manage().timeouts().implicitlyWait(200, TimeUnit.MILLISECONDS);
+        new WebDriverWait(driver, Duration.ofMillis(200));
         driver.manage().window().maximize();
         driver.get(PropertyReader.getWebsite());
         waitSeconds();
